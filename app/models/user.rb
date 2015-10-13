29b075791afb71729ee030490ef2ba564af7a36e
MdAqib <md.aqib@snapdeal.com>
2015-10-13 13:20:44 +0530

    adding password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
 	validates :name,  presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
-	format: { with: VALID_EMAIL_REGEX }
+	format: { with: VALID_EMAIL_REGEX },
+	uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 }
+	has_secure_password
 end
